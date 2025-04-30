cf5989a12770ec8a6f511d60b9aef1ac3df1cd2c
sebin-personal <hellosebinvincent@gmail.com>
2025-04-30 19:50:11 +0530

    Preprocccessed data

--- datasets/MSVD/transform_msvd_annotation.py
+++ datasets/MSVD/transform_msvd_annotation.py
@@ -33,15 +33,13 @@ with open(input_file, 'r', encoding='utf-8') as f:
     print("Total :", len(data.keys()))
 
     for video_id, captions in data.items():
-        if count > 10:
-            break
         for caption in captions:
-            if count <= 5:
+            if count <= 1200:
                 train.append({
                     "video": video_id,
                     "caption": caption
                 })
-            elif count <= 7:
+            elif count <= 1300:
                 val.append({
                     "video": video_id,
                     "caption": caption
@@ -67,4 +65,4 @@ with open(output_file, 'w', encoding='utf-8') as f:
 
 print(f"Number of train videos: {len(train)}")
 print(f"Number of val videos: {len(val)}")
-print(f"Number of test videos: {len(train)}")
+print(f"Number of test videos: {len(test)}")
