21f8c5b40d08e5f46d4440c1bf8093630ee9fcb1
sebin <hellosebinvincent@gmail.com>
2025-08-09 17:55:56 +0530

    Fix placeholder methods

--- src/layers/albert/modeling_albert.py
+++ src/layers/albert/modeling_albert.py
@@ -1485,6 +1485,358 @@ class AlbertForImageCaptioning(AlbertPreTrainedModel):
     def _do_output_past(self, outputs):
         return len(outputs) > 1
 
+    def _decode_step(self, input_ids, past):
+        model_inputs = self.prepare_inputs_for_generation(input_ids, past=past)
+        outputs = self(**model_inputs)  # (batch_size * num_beams, cur_len, vocab_size)
+        token_len = outputs[0].shape[1]
+        if self.od_labels_len == 0:
+            next_token_idx = token_len - 1
+        else:
+            if token_len == 2:
+                assert self._do_output_past(outputs)
+                next_token_idx = 1
+            else:
+                next_token_idx = token_len - self.od_labels_len - 1
+
+        next_token_logits = outputs[0][:, next_token_idx, :]  # (batch_size * num_beams, vocab_size)
+        assert outputs[0].shape[1] == model_inputs['input_ids'].shape[1]
+
+        # if model has past, then set the past variable to speed up decoding
+        if self._do_output_past(outputs):
+            past = outputs[1]
+        return next_token_logits, past
+
+    def _generate_no_beam_search(
+        self,
+        input_ids,
+        cur_len,
+        max_length,
+        do_sample,
+        temperature,
+        top_k,
+        top_p,
+        repetition_penalty,
+        pad_token_id,
+        eos_token_ids,
+        batch_size,
+    ):
+        """ Generate sequences for each example without beam search (num_beams == 1).
+            All returned sequence are generated independantly.
+        """
+        assert self.num_keep_best == 1, 'cannot generate >1 sentences in greedy search'
+        # current position / max lengths / length of generated sentences / unfinished sentences
+        unfinished_sents = []
+        if torch._C._get_tracing_state():
+            cur_unfinished = torch.ones(1, dtype=input_ids)
+        else:
+            cur_unfinished = input_ids.new(batch_size).fill_(1)
+
+        # log of scores for each sentence in the batch
+        logprobs = []
+
+        past = None
+
+        iteration=0
+
+        while cur_len < max_length:
+            iteration=iteration+1
+            model_inputs = self.prepare_inputs_for_generation(input_ids, past=past)
+            outputs = self(**model_inputs)
+            if cur_len == 1:
+                token_len = 2 + self.od_labels_len
+                next_token_idx = 1
+            else:
+                assert cur_len > 1
+                if not self._do_output_past(outputs):
+                    token_len = cur_len + 1 + self.od_labels_len
+                    next_token_idx = cur_len
+                else:
+                    token_len = 2
+                    next_token_idx = 1
+
+            assert outputs[0].shape[1] == token_len
+            next_token_logits = outputs[0][:, next_token_idx, :]
+
+            # if model has past, then set the past variable to speed up decoding
+            if self._do_output_past(outputs):
+                past = outputs[1]
+
+            # repetition penalty from CTRL paper (https://arxiv.org/abs/1909.05858)
+            if repetition_penalty != 1.0:
+                for i in range(batch_size):
+                    for previous_token in set(input_ids[i].tolist()):
+                        # if score < 0 then repetition penalty has to multiplied to reduce the previous token probability
+                        if next_token_logits[i, previous_token] < 0:
+                            next_token_logits[i, previous_token] *= repetition_penalty
+                        else:
+                            next_token_logits[i, previous_token] /= repetition_penalty
+
+            if do_sample:
+                # Temperature (higher temperature => more likely to sample low probability tokens)
+                if temperature != 1.0:
+                    next_token_logits = next_token_logits / temperature
+                # Top-p/top-k filtering
+                from .modeling_utils import top_k_top_p_filtering
+                next_token_logits = top_k_top_p_filtering(next_token_logits, top_k=top_k, top_p=top_p)
+                # Sample
+                next_token = torch.multinomial(F.softmax(next_token_logits, dim=-1), num_samples=1).squeeze(1)
+            else:
+                # Greedy decoding
+                next_token = torch.argmax(next_token_logits, dim=-1)
+
+            # Compute scores
+            _scores = F.log_softmax(next_token_logits, dim=-1)  # (batch_size, vocab_size)
+            _scores = torch.gather(_scores, -1, next_token.unsqueeze(-1))  # (batch_size, 1)
+            logprobs.append(_scores)  # (batch_size, 1)
+            unfinished_sents.append(cur_unfinished)
+
+            # update generations and finished sentences
+            tokens_to_add = next_token * cur_unfinished + pad_token_id * (1 - cur_unfinished)
+            input_ids = torch.cat([input_ids, tokens_to_add.unsqueeze(-1)], dim=-1)
+
+            for eos_token_id in eos_token_ids:
+                cur_unfinished = cur_unfinished.mul(tokens_to_add.ne(eos_token_id).long())
+            cur_len = cur_len + 1
+
+            # stop when there is a </s> in each sentence, or if we exceed the maximul length
+            if cur_unfinished.max() == 0:
+                break
+
+        # add eos_token_ids to unfinished sentences
+        if cur_len == max_length:
+            input_ids[:, -1].masked_fill_(cur_unfinished.to(dtype=torch.bool), eos_token_ids[0])
+
+        logprobs = torch.cat(logprobs, dim=1)
+        unfinished_sents = torch.stack(unfinished_sents, dim=1).float()
+        sum_logprobs = (logprobs * unfinished_sents).sum(dim=1)
+        # return logprobs to keep consistent with beam search output
+        logprobs = sum_logprobs / unfinished_sents.sum(dim=1)
+
+        # pad to the same length, otherwise DataParallel will give error
+        pad_len = max_length - input_ids.shape[1]
+        if pad_len > 0:
+            padding_ids = input_ids.new(batch_size, pad_len).fill_(pad_token_id)
+            input_ids = torch.cat([input_ids, padding_ids], dim=1)
+
+        # (batch_size, n_best, max_len), (batch_size, n_best)
+        return input_ids.unsqueeze(1), logprobs.unsqueeze(1)
+
+    def _generate_beam_search(
+        self,
+        input_ids,
+        cur_len,
+        max_length,
+        do_sample,
+        temperature,
+        top_k,
+        top_p,
+        repetition_penalty,
+        pad_token_id,
+        eos_token_ids,
+        batch_size,
+        length_penalty,
+        num_beams,
+        vocab_size,
+    ):
+        """ Generate sequences for each example with beam search.
+        """
+        from .modeling_utils import BeamHypotheses, top_k_top_p_filtering
+        
+        # Expand input to num beams
+        input_ids = input_ids.unsqueeze(1).expand(batch_size, num_beams, cur_len)
+        input_ids = input_ids.contiguous().view(batch_size * num_beams, cur_len)  # (batch_size * num_beams, cur_len)
+
+        # generated hypotheses
+        num_keep_best = self.num_keep_best
+        generated_hyps = [
+            BeamHypotheses(num_keep_best, max_length, length_penalty, early_stopping=False) for _ in range(batch_size)
+        ]
+        # NOTE: Expand >1 words to leave some spare tokens to keep the
+        # beam size, because some sentences may end here and cannot expand
+        # in the next level
+        TOPN_PER_BEAM = 2
+
+        # scores for each sentence in the beam
+        beam_scores = torch.zeros((batch_size, num_beams), dtype=torch.float, device=input_ids.device)
+        beam_scores[:, 1:] = -1e9
+        beam_scores = beam_scores.view(-1)  # shape (batch_size * num_beams,)
+
+        # cache compute states
+        past = None
+
+        # done sentences
+        done = [False for _ in range(batch_size)]
+
+        while cur_len < max_length:
+            model_inputs = self.prepare_inputs_for_generation(input_ids, past=past)
+            outputs = self(**model_inputs)  # (batch_size * num_beams, cur_len, vocab_size)
+            if cur_len == 1:
+                token_len = 2 + self.od_labels_len
+                next_token_idx = 1
+            else:
+                assert cur_len > 1
+                if not self._do_output_past(outputs):
+                    token_len = cur_len + 1 + self.od_labels_len
+                    next_token_idx = cur_len
+                else:
+                    token_len = 2
+                    next_token_idx = 1
+
+            assert outputs[0].shape[1] == token_len
+            scores = outputs[0][:, next_token_idx, :]  # (batch_size * num_beams, vocab_size)
+            assert outputs[0].shape[1] == model_inputs['input_ids'].shape[1]
+
+            # if model has past, then set the past variable to speed up decoding
+            if self._do_output_past(outputs):
+                past = outputs[1]
+
+            # repetition penalty (from CTRL paper https://arxiv.org/abs/1909.05858)
+            if repetition_penalty != 1.0:
+                for i in range(batch_size * num_beams):
+                    for previous_token in set(input_ids[i].tolist()):
+                        # if score < 0 then repetition penalty has to multiplied to reduce the previous token probability
+                        if scores[i, previous_token] < 0:
+                            scores[i, previous_token] *= repetition_penalty
+                        else:
+                            scores[i, previous_token] /= repetition_penalty
+
+            if do_sample:
+                # Temperature (higher temperature => more likely to sample low probability tokens)
+                if temperature != 1.0:
+                    scores = scores / temperature
+                # Top-p/top-k filtering
+                scores = top_k_top_p_filtering(
+                    scores, top_k=top_k, top_p=top_p, min_tokens_to_keep=2
+                )  # (batch_size * num_beams, vocab_size)
+                # Sample [TOPN_PER_BEAM] next words for each beam (so we have some spare tokens and match output of greedy beam search)
+                next_words = torch.multinomial(F.softmax(scores, dim=-1),
+                        num_samples=TOPN_PER_BEAM)  # (batch_size * num_beams, TOPN_PER_BEAM)
+                # Compute next scores
+                _scores = F.log_softmax(scores, dim=-1)  # (batch_size * num_beams, vocab_size)
+                _scores = torch.gather(_scores, -1, next_words)  # (batch_size * num_beams, TOPN_PER_BEAM)
+                next_scores = _scores + beam_scores[:, None].expand_as(_scores)  # (batch_size * num_beams, TOPN_PER_BEAM)
+                # Match shape of greedy beam search
+                beam_indices = torch.arange(num_beams) * vocab_size
+                beam_indices = beam_indices.repeat(batch_size, TOPN_PER_BEAM).to(next_words.device)
+                next_words = next_words.view(batch_size, TOPN_PER_BEAM * num_beams)  # (batch_size, TOPN_PER_BEAM * num_beams)
+                next_words = next_words + beam_indices
+                next_scores = next_scores.view(batch_size, TOPN_PER_BEAM * num_beams)  # (batch_size, TOPN_PER_BEAM * num_beams)
+            else:
+                # do greedy beam search
+                scores = F.log_softmax(scores, dim=-1)  # (batch_size * num_beams, vocab_size)
+                assert scores.size() == (batch_size * num_beams, vocab_size)
+                # Add the log prob of the new beams to the log prob of the beginning of the sequence (sum of logs == log of the product)
+                _scores = scores + beam_scores[:, None].expand_as(scores)  # (batch_size * num_beams, vocab_size)
+                # re-organize to group the beam together (we are keeping top hypothesis accross beams)
+                _scores = _scores.view(batch_size, num_beams * vocab_size)  # (batch_size, num_beams * vocab_size)
+                next_scores, next_words = torch.topk(_scores, TOPN_PER_BEAM * num_beams, dim=1, largest=True, sorted=True)
+
+            assert next_scores.size() == next_words.size() == (batch_size, TOPN_PER_BEAM * num_beams)
+
+            # next batch beam content
+            # list of (batch_size * num_beams) tuple(next hypothesis score, next word, current position in the batch)
+            next_batch_beam = []
+
+            # for each sentence
+            for batch_ex in range(batch_size):
+
+                # if we are done with this sentence
+                done[batch_ex] = done[batch_ex] or generated_hyps[batch_ex].is_done(next_scores[batch_ex].max().item())
+                if done[batch_ex]:
+                    next_batch_beam.extend([(0, pad_token_id, 0)] * num_beams)  # pad the batch
+                    continue
+
+                # next sentence beam content
+                next_sent_beam = []
+
+                # next words for this sentence
+                for idx, score in zip(next_words[batch_ex], next_scores[batch_ex]):
+
+                    # get beam and word IDs
+                    beam_id = idx // vocab_size
+                    word_id = idx % vocab_size
+
+                    # end of sentence, or next word
+                    if word_id.item() in eos_token_ids or cur_len + 1 == max_length:
+                        generated_hyps[batch_ex].add(
+                            input_ids[batch_ex * num_beams + beam_id, :cur_len].clone(), score.item()
+                        )
+                    else:
+                        next_sent_beam.append((score, word_id, batch_ex * num_beams + beam_id))
+
+                    # the beam for next step is full
+                    if len(next_sent_beam) == num_beams:
+                        break
+
+                # update next beam content
+                if cur_len + 1 == max_length:
+                    assert len(next_sent_beam) == 0
+                else:
+                    assert len(next_sent_beam) == num_beams
+
+                if len(next_sent_beam) == 0:
+                    next_sent_beam = [(0, pad_token_id, 0)] * num_beams  # pad the batch
+                next_batch_beam.extend(next_sent_beam)
+                assert len(next_batch_beam) == num_beams * (batch_ex + 1)
+
+            # sanity check / prepare next batch
+            assert len(next_batch_beam) == batch_size * num_beams
+            beam_scores = beam_scores.new([x[0] for x in next_batch_beam])
+            beam_words = input_ids.new([x[1] for x in next_batch_beam])
+            beam_idx = input_ids.new([x[2] for x in next_batch_beam])
+
+            # re-order batch
+            input_ids = input_ids[beam_idx, :]
+            input_ids = torch.cat([input_ids, beam_words.unsqueeze(1)], dim=-1)
+
+            # re-order internal states
+            if past:
+                reordered_past = []
+                for layer_past in past:
+                    # get the correct batch idx from layer past batch dim
+                    # batch dim of `past` and `mems` is at 1st position
+                    reordered_layer_past = [layer_past[i].unsqueeze(0).clone().detach() for i in beam_idx]
+                    reordered_layer_past = torch.cat(reordered_layer_past, dim=0)
+                    # check that shape matches
+                    assert reordered_layer_past.shape == layer_past.shape
+                    reordered_past.append(reordered_layer_past)
+                past = tuple(reordered_past)
+
+            # update current length
+            cur_len = cur_len + 1
+
+            # stop when we are done with each sentence
+            if all(done):
+                break
+
+        # select the best hypotheses
+        tgt_len = torch.ones(batch_size, num_keep_best, dtype=torch.long)
+        logprobs = torch.zeros(batch_size, num_keep_best,
+                dtype=torch.float).fill_(-1e5).to(input_ids.device)
+        all_best = []
+
+        for i, hypotheses in enumerate(generated_hyps):
+            best = []
+            hyp_scores = torch.tensor([x[0] for x in hypotheses.hyp])
+            _, best_indices = torch.topk(hyp_scores,
+                    min(num_keep_best, len(hyp_scores)), largest=True)
+            for best_idx, hyp_idx in enumerate(best_indices):
+                conf, best_hyp = hypotheses.hyp[hyp_idx]
+                best.append(best_hyp)
+                logprobs[i, best_idx] = conf
+                tgt_len[i, best_idx] = len(best_hyp) + 1  # +1 for the <EOS> symbol
+
+            all_best.append(best)
+
+        # generate target batch, pad to the same length
+        decoded = input_ids.new(batch_size, num_keep_best, max_length).fill_(pad_token_id)
+        for batch_idx, best in enumerate(all_best):
+            for best_idx, hypo in enumerate(best):
+                decoded[batch_idx, best_idx, : tgt_len[batch_idx, best_idx] - 1] = hypo
+                decoded[batch_idx, best_idx, tgt_len[batch_idx, best_idx] - 1] = eos_token_ids[0]
+
+        return decoded, logprobs
+
     def prod_generate(self, img_feats, od_label_ids, max_length,
             bos_token_id, eos_token_ids, mask_token_id, od_labels_start_posid,
             add_od_labels=True, cls_token_segment_id=0,
