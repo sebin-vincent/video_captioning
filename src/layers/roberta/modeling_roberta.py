1adeeabfe84238cb4117608d0f61cd57db1b8d07
sebin <hellosebinvincent@gmail.com>
2025-10-26 18:31:07 +0530

    Fix import issues

--- src/layers/roberta/modeling_roberta.py
+++ src/layers/roberta/modeling_roberta.py
@@ -241,7 +241,7 @@ class RobertaSelfAttention(nn.Module):
         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
         context_layer = context_layer.view(*new_context_layer_shape)
 
-        outputs = (context_layer, attention_probs) if self.output_attentions else (context_layer,)
+        outputs = (context_layer,)
         return outputs
 
 
